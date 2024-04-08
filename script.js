77ae5bdd699bf9b9bd13c7e376a5fc38d58f8fbd
crisscde <cristophercuautzo@gmail.com>
2024-04-08 14:13:54 -0600

    Add the function playGame()

--- script.js
+++ script.js
@@ -4,19 +4,55 @@ function getComputerChoice () {
   return CHOICES[Math.floor(Math.random() * 3)]
 }
 
+function getUserChoice () {
+  const USER = prompt("Rock, Paper, Scissors...!!!")
+  const PLAYER_CHOICE = USER.slice(0, 1).toUpperCase()
+    .concat(USER.slice(1).toLowerCase());
+  return PLAYER_CHOICE;
+}
 
-function playRound(playerSelection, computerSelection) {
-  let playerChoice = playerSelection.slice(0, 1).toUpperCase()
-    .concat(playerSelection.slice(1).toLowerCase())
-  if (playerChoice === computerSelection) {
+function playRound(user , computer) {
+  if (user === computer) {
     return "It was a tie!"
-  } else if((playerChoice === "Rock") && (computerSelection == "Scissors")) {
-    return `You win! ${playerChoice} beats ${computerSelection}`
-  } else if((playerChoice === "Scissors") && (computerSelection == "Paper")) {
-    return `You win! ${playerChoice} beats ${computerSelection}`
-  } else if((playerChoice === "Paper") && (computerSelection == "Rock")) {
-    return `You win! ${playerChoice} beats ${computerSelection}`
+  } else if((user === "Rock") && (computer == "Scissors")) {
+    return `You Win! ${user} beats ${computer}`
+  } else if((user === "Scissors") && (computer == "Paper")) {
+    return `You Win! ${user} beats ${computer}`
+  } else if((user === "Paper") && (computer == "Rock")) {
+    return `You Win! ${user} beats ${computer}`
   } else {
-    return `You Lose! ${computerSelection} beats ${playerChoice}`
+    return `You Lose! ${computer} beats ${user}`
+  }
+}
+
+function playGame() {
+  let computerScore = 0;
+  let userScore = 0;
+  
+  for (let round = 1; round <= 5; round++) {
+    console.log(`Round ${round}`)
+    let game = playRound(getUserChoice(), getComputerChoice());
+    let winner = game.slice(4, 8);
+    console.log(game)
+    if (winner === "Win!") {
+      userScore += 1
+    } else if (winner === "Lose") {
+      computerScore += 1
+    }
+  }
+
+  if (userScore === computerScore) {
+    console.log( "It was a tie!!" )
+  } else if (userScore > computerScore) {
+    console.log( `You Win!!! with a score of ${userScore} vs ${computerScore}` )
+  } else {
+    console.log( `You Lose!!! with a score of ${userScore} vs ${computerScore}` )
+  }
+
+  let again = prompt("Do you want to play again??? ")
+  if (again.toLowerCase() === "yes") {
+    playGame()  
+  } else {
+    return "Goodbye!! I think was a good game, see you later"
   }
 }
